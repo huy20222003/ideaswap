3298950b6057c44a7829ee52df6f663b264247e3
huy20222003 <120671495+huy20222003@users.noreply.github.com>
2024-05-22 16:03:34 +0700

    Update FormDialogUploadDocument.jsx

--- src/Components/FormDialog/document/FormDialogUploadDocument/FormDialogUploadDocument.jsx
+++ src/Components/FormDialog/document/FormDialogUploadDocument/FormDialogUploadDocument.jsx
@@ -79,7 +79,7 @@ const FormDialogUploadDocument = () => {
 
         // Sử dụng Axios để gửi dữ liệu form data
         const response = await axios.post(
-          'http://localhost:3000/api/v1/document/add',
+          'https://ideaswap-server.onrender.com/api/v1/document/add',
           formData,
           {
             headers: {
