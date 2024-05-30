fccbc67888b4ef281eb169bf3490713785e4bda7
huy20222003 <120671495+huy20222003@users.noreply.github.com>
2024-05-30 08:16:53 +0700

    Update LoginForm.jsx

--- src/section/auth/login/LoginForm.jsx
+++ src/section/auth/login/LoginForm.jsx
@@ -60,7 +60,11 @@ const LoginForm = () => {
       password: yup
         .string()
         .required('Password is required')
-        .min(7),
+        .min(7)
+        .matches(
+          /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$%^&+=]).{7,}$/,
+          'Minimum password consists of 7 characters, with uppercase letters, lowercase letters, numbers and special characters'
+        ),
     }),
     onSubmit: async (values) => {
       try {
